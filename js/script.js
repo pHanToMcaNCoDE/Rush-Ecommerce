db773a358072011e5c5f3435c1d0ac48ebdb84a6
Victory Odumeh <vodumeh@gmail.com>
2023-04-15 03:42:37 +0100

    added a new js file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// var error = document.querySelector('.errorData');
+// var uname = document.querySelector('.uname');
+// var email = document.querySelector('.email');
+// var pwd = document.querySelector('.pwd');
+// var formReg = document.querySelector('.submitReg');
+
+// formReg.addEventListener('onclick', () =>{
+    // if(uname.nodeValue == "" && email.nodeValue == "" && pwd.nodeValue == ""){
+        // error.classList.add('active');
+        // error.style.transition = "all .6s";
+    // }
+// })
+
