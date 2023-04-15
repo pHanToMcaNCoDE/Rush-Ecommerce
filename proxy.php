8798c680d61d99adda144c590bf5803db668e89b
Victory Odumeh <vodumeh@gmail.com>
2023-04-15 03:41:49 +0100

    Tackling errors

--- proxy.php
+++ proxy.php
@@ -1,7 +1,12 @@
 <?php
 
+
+
 require("connect.php");
 require("data.php");
+require("loginData.php");
+
+// $connect = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
 
 // session_start();
 
@@ -26,16 +31,57 @@ if(isset($_GET['signup']) && $_GET['signup'] == "done" && isset($_POST['submit']
                     header("Location: Forms.php?signup=email");
                     exit();
                 }else{
-
-                if($reg->getRegistration($uname, $email, $pwd)){
-                    header("Location: Forms.php?signup=success");
-                    exit();
-                }else{
-                    header("Location: Forms.php?signup=error");
-                }
-                    
+                    if($reg->getRegistration($uname, $email, $pwd)){
+                        header("Location: Forms.php?signup=success");
+                        exit();
+                    }else{
+                        header("Location: Forms.php?signup=error");
+                    }
                 } 
             }
+                
+}
+
+
+// Login
+
+if(isset($_GET['signin']) && $_GET['signin'] == "done" && isset($_POST['submit'])){
+
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+
+    $log = new Login();
+    
+    // $uname = $_POST['uname'];
+
+
+    if($log->getLogin($email, $pwd)){
+        header("Location: index.php");
+        // exit();
+    }else{
+        echo "Error";
+    }
+    
+    // include_once 'connect.php';
+
+            // if(empty($email) || empty($pwd)){
+            //     header("Location: Forms.php?signin=empty");
+            //     exit();
+            // }else{
+
+            //     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            //         header("Location: Forms.php?signin=email");
+            //         exit();
+            //     }else{
+            //         if($log->getLogin($email, $pwd)){
+            //             header("Location: index.php?signin=success");
+            //             exit();
+            //         }else{
+            //             header("Location: Forms.php?signin=error");
+            //             exit();
+            //         }
+            //     } 
+            // }
                 
 }
 
