8d681725420a32f197f0bb88e4cac7651f5eaf9c
Victory Odumeh <vodumeh@gmail.com>
2023-04-15 11:03:21 +0100

    Starting with the commerce site proper

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    require("commerceNav.php");
 
 ?>
 
@@ -14,8 +14,53 @@
     <link rel="stylesheet" href="styles/style.css">
 </head>
 
-<body>
-    <h2>Dashboard</h2>
+<body class="homeIn">
+    <section class="categ" id="categ">
+        <div class="max-width">
+            <div class="category">
+                <h3>Categories</h3>
+                <div class="content">
+                    <div class="icon">
+                        <img src="assets/categIcons/electronics.png" alt="Electronics Icon" srcset="" width="35" height="35">
+                    </div>
+                    <div class="txt">
+                        <a href="#"><p>Electronics</p></a>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <img src="assets/categIcons/medicine.png" alt="Medicine Icon" srcset="" width="35" height="35">
+                    </div>
+                    <div class="txt">
+                        <a href="#"><p>Medicine</p></a>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <img src="assets/categIcons/home-life.png" alt="Lifestyle Icon" srcset="" width="35" height="35">
+                    </div>
+                    <div class="txt">
+                        <a href="#"><p>Home & Lifestyle</p></a>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <img src="assets/categIcons/sports.png" alt="Sports Icon" srcset="" width="35" height="35">
+                    </div>
+                    <div class="txt">
+                        <a href="#"><p>Sports & Outdoors</p></a>
+                    </div>
+                </div>
+                <div class="content">
+                    <div class="icon">
+                        <img src="assets/categIcons/fashion.png" alt="Fashion Icon" srcset="" width="35" height="35">
+                    </div>
+                    <div class="txt">
+                        <a href="#"><p>Fashion</p></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
-
 </html>
