928f75b8a083c2ab09ee972ce863e902eb1fd6ef
Victory Odumeh <vodumeh@gmail.com>
2023-04-15 01:32:46 +0100

    Completed validation for registration form

--- proxy.php
+++ proxy.php
@@ -1,51 +1,61 @@
 <?php
 
-// require("connect.php");
-// require("data.php");
+require("connect.php");
+require("data.php");
 
 // session_start();
 
 // // Registration
 
-// if(isset($_GET['register']) && $_GET['register'] == "done" && isset($_POST['submit'])){
+if(isset($_GET['signup']) && $_GET['signup'] == "done" && isset($_POST['submit'])){
     
-//     $reg = new Registration();
+    $reg = new Registration();
     
+    $uname = $_POST['uname'];
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+    
+    // include_once 'connect.php';
+
+            if(empty($uname) || empty($email) || empty($pwd)){
+                header("Location: Forms.php?signup=empty");
+                exit();
+            }else{
+
+                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                    header("Location: Forms.php?signup=email");
+                    exit();
+                }else{
+
+                if($reg->getRegistration($uname, $email, $pwd)){
+                    header("Location: Forms.php?signup=success");
+                    exit();
+                }else{
+                    header("Location: Forms.php?signup=error");
+                }
+                    
+                } 
+            }
+                
+}
+
+// if(isset($_POST['submit'])){
+    
+//     include_once 'connect.php';
+
 //     $uname = $_POST['uname'];
 //     $email = $_POST['email'];
 //     $pwd = $_POST['pwd'];
 
-//     if($reg->getRegistration($uname, $email, $pwd)){
-//         // $_SESSION['status'] = "You've registered successfully";
-//         // $_SESSION['text'] = "Try and Login";
-//         // $_SESSION['success'] = "success";
-//         header("Location: Forms.php");
+//     if(empty($uname) || empty($email) || empty($pwd)){
+//         header("Location: Forms.php?msg=empty");
+//         exit();
 //     }else{
-//         // $_SESSION['status'] = "Something went wrong";
-//         // $_SESSION['text'] = "Try again";
-//         // $_SESSION['success'] = "error";
-//         header("Location: Forms.php");
+//         if(!preg_match("/^[a-zA-Z]*$/", $uname)){
+//             header("Location: Forms.php?msg=string");
+//         }
 //     }
 // }
-
-
-if(isset($_POST['submit'])){
-    
-    include_once 'connect.php';
-
-    $uname = $_POST['uname'];
-    $email = $_POST['email'];
-    $pwd = $_POST['pwd'];
-
-    if(empty($uname) || empty($email) || empty($pwd)){
-        header("Location: Forms.php?msg=empty");
-        exit();
-    }else{
-        if(!preg_match("/^[a-zA-Z]*$/", $uname)){
-            header("Location: Forms.php?msg=string");
-        }
-    }
-}
 
 
 ?>
