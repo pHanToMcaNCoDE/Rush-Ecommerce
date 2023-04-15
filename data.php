d4f1b45f2dbbc05f492688aee30a8ea3f4bbbde4
Victory Odumeh <vodumeh@gmail.com>
2023-04-15 01:33:38 +0100

    Starting with login form:

--- data.php
+++ data.php
@@ -25,5 +25,31 @@ class Registration{
 }
 
 
+// Login
+
+class Login{
+    
+    public function getLogin($email, $pwd){
+        
+        $connection = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
+
+        if($connection->connect_error){
+            die("Cannot connect to the database ".$connection->connect_error);
+        }else{
+            
+            $sql = "SELECT * FROM `Registration`(`UserName`, `Email_Address`, `Password`) WHERE `Email_Address` = '$email' AND `Password` = '$pwd';";
+
+            $result = $connection->query($sql);
+            
+            
+            if($row = $result->fetch_assoc()){
+                
+                $_SESSION['UNAME'] = $row['UserName'];
+                $_SESSION['EMAIL'] = $row['Email_Address'];
+            }
+        }
+        $connection->close();
+    }
+}
 
 ?>
