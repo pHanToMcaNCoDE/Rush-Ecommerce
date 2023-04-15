70185b7c0da66c80ad3c4512378ad6b09530faea
Victory Odumeh <vodumeh@gmail.com>
2023-04-15 11:07:25 +0100

    for Forms page

---
+++ commerceNav.php
@@ -0,0 +1,60 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Rush - inspired by dreams || Welcome</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <div class="commerceNav">
+        <div class="max-width">
+            <div class="logo">
+                <a href="index.php"><img src="assets/logo.png" alt="Official Logo"></a>
+            </div>
+            <ul class="navigate">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <div class="search">
+                <input type="text" name="search" placeholder="e.g; Console">
+                <a  href="#" class="searchGo"><img src="assets/s.png" alt="Search Icon" width="30" height="30"></a>
+            </div>
+            <div class="commerceIcons">
+                <ul class="theIcons">
+                    <li>
+                        <abbr title="Account" style="padding: 5px; font-size: 20px;">
+                            <a href="#">
+                                <img src="assets/user.png" alt="Account Icon" width="30" height="30">
+                            </a>
+                        </abbr>
+                    </li>
+                    <li>
+                        <abbr title="Favourite" style="padding: 5px; font-size: 20px;">
+                            <a href="#">
+                                <img src="assets/like.png" alt="Like-List Icon" width="30" height="30">
+                            </a>
+                        </abbr>
+                    </li>
+                    <li>
+                        <abbr title="Cart" style="padding: 5px; font-size: 20px;">
+                            <a href="#">
+                                <img src="assets/cart.png" alt="Cart Icon" width="30" height="30">
+                            </a>
+                        </abbr>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
