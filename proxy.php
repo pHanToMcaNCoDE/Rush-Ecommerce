16430d909f450f24c40ff65b404315a622f16c25
Victory Odumeh <vodumeh@gmail.com>
2023-05-14 10:27:17 +0100

    will also be deleted soon

--- proxy.php
+++ proxy.php
@@ -3,39 +3,33 @@
 
 
 require("connect.php");
-require("data.php");
 require("loginData.php");
 
-// $connect = new mysqli(SERVER, USERNAME, PASSWORD, DATABASE);
 
-// session_start();
 
-// // Registration
+// Login
 
-if(isset($_GET['signup']) && $_GET['signup'] == "done" && isset($_POST['submit'])){
-    
-    $reg = new Registration();
-    
+if(isset($_GET['signin']) && $_GET['signin'] == "done" && isset($_POST['submit'])){
+
     $uname = $_POST['uname'];
-    $email = $_POST['email'];
     $pwd = $_POST['pwd'];
+
+    $log = new Login();
     
-    // include_once 'connect.php';
 
             if(empty($uname) || empty($email) || empty($pwd)){
-                header("Location: Forms.php?signup=empty");
+                header("Location: Login.php?signin=empty");
                 exit();
             }else{
-
-                if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                    header("Location: Forms.php?signup=email");
+                if(strlen($pwd) > 8){
+                    header("Location: Login.php?signin=password");
                     exit();
                 }else{
-                    if($reg->getRegistration($uname, $email, $pwd)){
-                        header("Location: Forms.php?signup=success");
+                    if($log->getLogin($uname, $pwd)){
+                        header("Location: index.php?signin=success");
                         exit();
                     }else{
-                        header("Location: Forms.php?signup=error");
+                        header("Location: Login.php?sigin=error");
                     }
                 } 
             }
@@ -43,65 +37,6 @@ if(isset($_GET['signup']) && $_GET['signup'] == "done" && isset($_POST['submit']
 }
 
 
-// Login
-
-if(isset($_GET['signin']) && $_GET['signin'] == "done" && isset($_POST['submit'])){
-
-    $email = $_POST['email'];
-    $pwd = $_POST['pwd'];
-
-    $log = new Login();
-    
-    // $uname = $_POST['uname'];
-
-
-    if($log->getLogin($email, $pwd)){
-        header("Location: index.php");
-        // exit();
-    }else{
-        echo "Error";
-    }
-    
-    // include_once 'connect.php';
-
-            // if(empty($email) || empty($pwd)){
-            //     header("Location: Forms.php?signin=empty");
-            //     exit();
-            // }else{
-
-            //     if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-            //         header("Location: Forms.php?signin=email");
-            //         exit();
-            //     }else{
-            //         if($log->getLogin($email, $pwd)){
-            //             header("Location: index.php?signin=success");
-            //             exit();
-            //         }else{
-            //             header("Location: Forms.php?signin=error");
-            //             exit();
-            //         }
-            //     } 
-            // }
-                
-}
-
-// if(isset($_POST['submit'])){
-    
-//     include_once 'connect.php';
-
-//     $uname = $_POST['uname'];
-//     $email = $_POST['email'];
-//     $pwd = $_POST['pwd'];
-
-//     if(empty($uname) || empty($email) || empty($pwd)){
-//         header("Location: Forms.php?msg=empty");
-//         exit();
-//     }else{
-//         if(!preg_match("/^[a-zA-Z]*$/", $uname)){
-//             header("Location: Forms.php?msg=string");
-//         }
-//     }
-// }
 
 
 ?>
