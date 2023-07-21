7b488fedc166c0bf251f57c4ec2702158aa536d6
Victory Odumeh <vodumeh@gmail.com>
2023-07-21 02:40:40 +0100

    .

--- footer.php
+++ footer.php
@@ -4,41 +4,78 @@
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="csss/style.css">
 </head>
 
-<section class="footer" id="footer">
-    <div class="max-width">
-        <h3>Niger street P.O 12333461, </br>Fegge, Kano Nigeria</h3>
-        <div class="links">
-            <h4>Links</h4>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Shop</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </div>
-        <div class="help">
-            <h4>Help</h4>
-            <ul>
-                <li><a href="#">Payment option</a></li>
-                <li><a href="#">Returns</a></li>
-                <li><a href="#">Privacy policies</a></li>
-            </ul>
-        </div>
-        <div class="newsletter">
-            <h4>Newsletter</h4>
-            <div class="input">
-                <input type=" submit" value="">
-                <a href="#">SUBSCRIBE</a>
+<body>
+    <section class="footer" id="footer">
+        <div class="max-width">
+            <h3>Niger street P.O 12333461, </br>Fegge, Kano Nigeria</h3>
+            <div class="links">
+                <h4>Links</h4>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Shop</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="links">
+                <h4>Lorem</h4>
+                <ul>
+                    <li><a href="#">lorem ipsum</a></li>
+                    <li><a href="#">lorem ipppsum</a></li>
+                    <li><a href="#">loremm ispum</a></li>
+                    <li><a href="#">ipsum lorem</a></li>
+                </ul>
+            </div>
+            <div class="links">
+                <h4>Ipsum</h4>
+                <ul>
+                    <li><a href="#">ipsum Lorem</a></li>
+                    <li><a href="#">ipsum Lorem</a></li>
+                    <li><a href="#">ipsum Lorem</a></li>
+                    <li><a href="#">ipsum Lorem</a></li>
+                    <li><a href="#">ipsum Lorem</a></li>
+                </ul>
+            </div>
+            <div class="help">
+                <h4>Help</h4>
+                <ul>
+                    <li><a href="#">Payment option</a></li>
+                    <li><a href="#">Returns</a></li>
+                    <li><a href="#">Privacy policies</a></li>
+                </ul>
+            </div>
+
+            <div class="newsletter">
+                <h4>Newsletter</h4>
+                <div class="input">
+                    <input type=" submit" value="">
+                    <a href="#">SUBSCRIBE</a>
+                </div>
+                <ul class="socials">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         </div>
-    </div>
-    <div class="line"></div>
-    <div class=" down">
-        <p>&copy;<?php echo date("Y") ?> all rights reserved</p>
-    </div>
-</section>
+        <div class="line"></div>
+        <div class=" down">
+            <p>&copy;<?php echo date("Y") ?> all rights reserved</p>
+        </div>
+    </section>
+</body>
+
+</html>
