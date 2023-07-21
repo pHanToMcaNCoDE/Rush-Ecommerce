24e5e9c6e75f94f977a5d6368b482a2c9abbb79c
Victory Odumeh <vodumeh@gmail.com>
2023-07-21 02:10:38 +0100

    .

--- footer.php
+++ footer.php
@@ -4,51 +4,41 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="csss/style.css">
 </head>
 
-<body>
-    <section class="footer" id="footer">
-        <div class="max-width">
-            <h3>Niger street P.O 12333461, </br>Fegge, Kano Nigeria</h3>
-            <div class="links">
-                <h4>Links</h4>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Shop</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </div>
-            <div class="help">
-                <h4>Help</h4>
-                <ul>
-                    <li><a href="#">Payment option</a></li>
-                    <li><a href="#">Returns</a></li>
-                    <li><a href="#">Privacy policies</a></li>
-                </ul>
-            </div>
-            <div class="newsletter">
-                <h4>Newsletter</h4>
-                <div class="input">
-                    <input type=" submit" value="">
-                    <a href="#">SUBSCRIBE</a>
-                </div>
+<section class="footer" id="footer">
+    <div class="max-width">
+        <h3>Niger street P.O 12333461, </br>Fegge, Kano Nigeria</h3>
+        <div class="links">
+            <h4>Links</h4>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        <div class="help">
+            <h4>Help</h4>
+            <ul>
+                <li><a href="#">Payment option</a></li>
+                <li><a href="#">Returns</a></li>
+                <li><a href="#">Privacy policies</a></li>
+            </ul>
+        </div>
+        <div class="newsletter">
+            <h4>Newsletter</h4>
+            <div class="input">
+                <input type=" submit" value="">
+                <a href="#">SUBSCRIBE</a>
             </div>
         </div>
-        <div class="line"></div>
-        <div class=" down">
-            <p>&copy;<?php echo date("Y") ?> all rights reserved</p>
-        </div>
-    </section>
-</body>
-
-</html>
+    </div>
+    <div class="line"></div>
+    <div class=" down">
+        <p>&copy;<?php echo date("Y") ?> all rights reserved</p>
+    </div>
+</section>
