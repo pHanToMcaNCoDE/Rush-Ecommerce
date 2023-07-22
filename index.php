9cc0f2c55dd3ca67d6a3d1780b1f4cf3116cf9cf
Victory Odumeh <vodumeh@gmail.com>
2023-07-22 20:06:14 +0100

    .

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-    require("commerceNav.php");
 
-    
-    ini_set("display_errors", "on");
+require("navbar.php");
+require("data.php");
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -13,101 +11,71 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link href="styles/style.css" rel="stylesheet" /> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <title>The Rush || inspired by dreams - Registration</title>
+    <title>The Rush -- inspired by dreams || Forms</title>
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://kit.fontawesome.com/032421aa45.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-    <section class="categ" id="categ">
+    <section id="regForm" class="regForm">
+        <img src="assets/regImg.jpg" alt="Product Image" />
         <div class="max-width">
-            <div class="left">
-                <div class="container">
-                    <h2>CATEGORY</h2>
-                    <ul class="cat">
-                        <li>
-                            <a href="#"><img src="assets/categIcons/electronics.png" alt="">Electronics</a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="assets/categIcons/fashion.png" alt="">Fashion</a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="assets/categIcons/groceries.png" alt="">Groceries</a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="assets/categIcons/home-life.png" alt="">LifeStyle</a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="assets/categIcons/medicine.png" alt="">Medicine</a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="assets/categIcons/pets.png" alt="">Pets</a>
-                        </li>
-                        <li>
-                            <a href="#"><img src="assets/categIcons/sports.png" alt="">Outdoors</a>
-                        </li>
-                    </ul>
+            <div class="up">
+                <div class="img">
+                    <img src="assets/logo.png" alt="Rush Logo">
+                </div>
+                <div class="txt">
+                    <h2>My Account</h2>
+                    <p><a href="index.php">My Account</a> > Signup</p>
                 </div>
             </div>
-            <div class="right">
-                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
-                            class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
-                    </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="assets/ads/adv1.jpg" class="d-block" alt="Advert1">
-                            <div class="textOne">
-                                <h2>Get yours at $1.99</h2>
-                                <p>You can get your favourite snacks at <br>affordable prices</p>
-                                <a href="#">Order now <i style="margin-left: 5px;"
-                                        class="fa-solid fa-arrow-right-long"></i></a>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/ads/adv2.jpg" class="d-block" alt="Advert2">
-                            <div class="textTwo">
-                                <h2>Get high quality <br>Cosmetics!</h2>
-                                <a href="#">Order now <i style="margin-left: 5px;"
-                                        class="fa-solid fa-arrow-right-long"></i></a>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/ads/adv3.jpg" class="d-block" alt="Advert3">
-                            <div class="textThree">
-                                <h2>At affordable prices!</h2>
-                                <p>Rush down and grab your kids favourite <br>heros at lesser prices.
-                                </p>
-                                <a href="#">Order now <i style="margin-left: 5px;"
-                                        class="fa-solid fa-arrow-right-long"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-    </section>
-    <?php require "grid.php"; ?>
 
+            <div class="down">
+                <form class="register" action="" method="post">
+                    <h2>Signup</h2>
+                    <div class="row">
+                        <div class="content">
+                            <label for="Username">Username:</label>
+                            <input type=" text" name="uname" placeholder="e.g; Jdoe12">
+                        </div>
+                        <div class="content">
+                            <label for="Email Address">Email address:</label>
+                            <input type="text" name="email" placeholder="e.g; jDoe@gmail.com">
+                        </div>
+                        <div class="content">
+                            <label for="Password">Password:</label>
+                            <input type="password" name="pwd" placeholder="jD123">
+                        </div>
+                    </div>
+                    <div class="btn">
+                        <input type="submit" name="submit" value="Register" class="submitReg">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <?php
+    require("footer.php");
+    ?>
+    <script>
+    var success = document.getElementById('suc');
+    var error = document.getElementById('err');
+    var del = document.querySelectorAll('.fa-xmark');
+
+
+    del.addEventListener('click', () => {
+            success.classList.add('active');
+        }
+
+    );
+
+    del.addEventListener('click', () => {
+            error.classList.add('active');
+        }
+
+    );
+    </script>
 </body>
 
 </html>
